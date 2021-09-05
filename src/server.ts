200b0ec4a5361eae2e587c9751e82a8f4cc86cb4
baratarthur <arthurpbarata@gmail.com>
2021-09-05 20:01:25 -0300

    teste com porta em enviroment

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,8 @@ import swaggerUi from 'swagger-ui-express';
 
 import swaggerFile from './swagger.json';
 
+const port = process.env.PORT || 5000;
+
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
-app.listen(5000, () => console.log("Server is running!"));
+app.listen(port, () => console.log("Server is running!"));
