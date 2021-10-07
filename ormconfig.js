8f9bdc5392811d20ee3d688b41093fb2e6aef003
baratarthur <arthurpbarata@gmail.com>
2021-10-07 00:24:49 -0300

    teste de conxao com o banco

--- ormconfig.js
+++ ormconfig.js
@@ -1,10 +1,7 @@
 module.exports = {
   type: "postgres",
-  host: process.env.DATABASE_HOST,
+  URL: process.env.DATABASE_URL,
   port: 5432,
-  username: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
   migrations: ["./src/database/migrations/*.ts"],
   entities: ["./src/modules/**/entities/*.ts"],
   cli: {
