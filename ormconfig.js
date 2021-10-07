52e70a50a388df1d2f6aaaf691a251a789c20907
baratarthur <arthurpbarata@gmail.com>
2021-10-07 00:17:41 -0300

    adicionadas variáveis para conexão com o banco

---
+++ ormconfig.js
@@ -0,0 +1,18 @@
+module.exports = {
+  type: "postgres",
+  host: process.env.DATABASE_HOST,
+  port: 5432,
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  migrations: ["./src/database/migrations/*.ts"],
+  entities: ["./src/modules/**/entities/*.ts"],
+  cli: {
+    migrationsDir: "./src/database/migrations",
+  },
+  extra: {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
+};
